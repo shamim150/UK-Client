a0ae098050b19fec1dfe6fdd0d88dacabe99b68a
shamim150 <smshamimmahmud150@gmail.com>
2024-10-31 19:09:17 +0600

    color

--- components/Client.jsx
+++ components/Client.jsx
@@ -1,43 +1,28 @@
-import React from 'react'
-import logo1 from '../app/assets/logo.jpg'
-import Image from 'next/image'
+import React from 'react';
+import logo1 from '../app/assets/logo.jpg';
+import Image from 'next/image';
 
 const Client = () => {
   return (
-    <div className='p-1 md:p-2 '>
-<p className='text-white text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px] text-center font-bold '>$TEA</p>
-
-
-
-
-      {/* <div className='flex items-center justify-center flex-wrap gap-4 md:gap-6'>
-        <div className='flex items-center gap-x-2'>
-          <Image className='w-16 h-16 md:w-20 md:h-20 rounded-xl' alt='image1' src={logo1} />
-          <p className='text-white font-bold text-lg md:text-2xl'>PinkSale</p>
-        </div>
-        <div className='flex items-center gap-x-2'>
-          <Image className='w-16 h-16 md:w-20 md:h-20 rounded-xl' alt='image1' src={logo1} />
-          <p className='text-white font-bold text-lg md:text-2xl'>PinkSale</p>
-        </div>
-        <div className='flex items-center gap-x-2'>
-          <Image className='w-16 h-16 md:w-20 md:h-20 rounded-xl' alt='image1' src={logo1} />
-          <p className='text-white font-bold text-lg md:text-2xl'>PinkSale</p>
-        </div>
-        <div className='flex items-center gap-x-2'>
-          <Image className='w-16 h-16 md:w-20 md:h-20 rounded-xl' alt='image1' src={logo1} />
-          <p className='text-white font-bold text-lg md:text-2xl'>PinkSale</p>
-        </div>
-        <div className='flex items-center gap-x-2'>
-          <Image className='w-16 h-16 md:w-20 md:h-20 rounded-xl' alt='image1' src={logo1} />
-          <p className='text-white font-bold text-lg md:text-2xl'>PinkSale</p>
-        </div>
-        <div className='flex items-center gap-x-2'>
-          <Image className='w-16 h-16 md:w-20 md:h-20 rounded-xl' alt='image1' src={logo1} />
-          <p className='text-white font-bold text-lg md:text-2xl'>PinkSale</p>
-        </div>
-      </div> */}
+    <div className="p-1 md:p-2">
+      <p
+        className="text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px] text-center font-bold text-transparent"
+        style={{
+          WebkitTextStroke: "2px #f5bcba", // Lighter pink border color
+          color: "#c8a8e9", // Purple text color
+          textShadow: `
+            0 0 10px #f9a1a0, 
+            0 0 20px #f9a1a0, 
+            0 0 30px #f9a1a0, 
+            0 0 40px #f9a1a0, 
+            0 0 50px #f9a1a0, 
+            0 0 60px #f9a1a0`, // Multiple layers for stronger glow
+        }}
+      >
+        <span style={{ color: "#c8a8e9" }}>$</span>TEA
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Client
+export default Client;
