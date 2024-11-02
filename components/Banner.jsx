f463b5e45b927a6a00c0301c2b2b627db907d31e
shamim150 <smshamimmahmud150@gmail.com>
2024-11-02 10:39:42 +0600

    add

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -10,10 +10,8 @@ const Banner = () => {
       <Image
         className="sm:object-cover bg-no-repeat"
         alt="img"
-        // src={image1}
         priority
       />
-      {/* <div className="absolute left-0 -bottom-10 bg-gradient-to-b from-cyan-400/0 via-[20%] to-[#3663B5] h-[20vh] w-full"/> */}
     </div>
   );
 };
